da486843fcaa78a57cee4c198f07dd28aa3f82ee
Aaron Habacub Salguero Ceron <salguero.aaron@gmail.com>
2026-07-29 19:24:31 -0600

    feat: Existia error de not null archivos pendientes de hacer push

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 const boton =document.getElementById('menuBtn');
 const links = document.getElementById('nav__links');
-boton.addEventListener('click', () => {
-    links.classList.toggle('is-open');
+boton?.addEventListener('click', () => {
+    links?.classList.toggle('is-open');
 });
 
 console.log('Landing de emprendedores lista para la entrega 1 Ing. Guilmar, y pues arriba Argentina.')
